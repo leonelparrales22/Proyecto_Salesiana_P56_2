6193196a0bfafff761b946d05b1e74112f11e6b0
leonelparrales22 <dparrales@est.ups.edu.ec>
2020-06-14 15:40:49 -0500

    cambios

--- app_textbloob.py
+++ app_textbloob.py
@@ -66,5 +66,37 @@ def run_textbloob2():
     toCSV("results/clasificacion_textbloob.csv", data)
     print("Resultados Generados!")
 
+
+def run_textbloob3():
+    ########VADER#########
+    # !pip install vadersentiment
+    from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+    import pandas as pd
+    data = pd.read_csv("model/TWEETS/tweets.csv", error_bad_lines=False)
+    tweets = data[data.columns[1]]
+    analyzer = SentimentIntensityAnalyzer()
+    contador_positivos = 0
+    contador_negativos = 0
+    contador_neutros = 0
+    contador = 1
+    for texto in tweets:
+        sentimiento = analyzer.polarity_scores(texto)['compound']
+        sentimiento_texto = ""
+        if 0 <= sentimiento <= 0.4:
+            sentimiento_texto = "NEGATIVO"
+            contador_negativos += 1
+            print(str(sentimiento))
+        if 0.6 <= sentimiento <= 1:
+            sentimiento_texto = "POSITIVO"
+            contador_positivos += 1
+            print(str(sentimiento))
+        if 0.4 < sentimiento < 0.6:
+            sentimiento_texto = "NEUTRO"
+            contador_neutros += 1
+            print(str(sentimiento))
+        # data.append(str(contador) + ";" + sentimiento_texto + ";" + str(texto).replace('\n', ' '))
+
+
 # run_textbloob()
 # run_textbloob2()
+# run_textbloob3()
