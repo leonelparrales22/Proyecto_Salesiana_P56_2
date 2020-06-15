79b63288f9579b9a1d4447ca19449404c056fa15
leonelparrales22 <dparrales@est.ups.edu.ec>
2020-06-15 14:57:38 -0500

    cambio

--- app_arbol_desicion.py
+++ app_arbol_desicion.py
@@ -4,7 +4,7 @@ from controller.nlp import *
 import pandas as pd
 
 # EXTRACCIÓN DE TWEETS
-data = pd.read_csv("results/clasificacion_arboles_2.csv", error_bad_lines=False)
+data = pd.read_csv("results/final.csv.csv", error_bad_lines=False)
 tweets = data[data.columns[2]]
 sentimiento = data[data.columns[1]]
 sentimiento = sentimiento.values.tolist()
@@ -20,4 +20,3 @@ data = clasificacion_arbol_de_desicion(tweets, respaldo, sentimiento)
 # PASAR A CSV
 toCSV2("results/clasificacion_arboles.csv", data)
 print("Resultados Generados!")
-
